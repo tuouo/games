ba9e4ad459aa16272b1215f228d9d9c2a375776f
young luo <luotengyang@outlook.com>
2019-10-31 23:07:35 +0800

    reduce unnecessary recursion. seems to use sudoku skill if need more improvement

--- sodoku/sudo.py
+++ sodoku/sudo.py
@@ -4,10 +4,12 @@
 # for optimization
 
 
-class Solution(object):
-    def solveSudoku(self, board):
+class Solution:
+    def __init__(self, board):
         self.board = board
         self.candidates = self.get_candidates()
+
+    def solve_sudoku(self):
         # trick can be use here, then deal with complex situations with self.solver()
         self.solver()
 
@@ -27,7 +29,22 @@ class Solution(object):
         for i, j in candidate.keys():
             exist = get[(i // 3, j // 3)] + get[("c", j)] + get[("r", i)]
             candidate[(i, j)] = [digit for digit in digits if digit not in exist]
-        return candidate
+        return self.put_sure_candidate(candidate)
+
+    def put_sure_candidate(self, candidates):
+        i, j = min(candidates.keys(), key=lambda x: len(candidates[x]))
+        only_one_candidate = len(candidates[(i, j)]) == 1
+        while only_one_candidate:
+            num = candidates[(i, j)][0]
+            self.board[i][j] = num
+            del candidates[(i, j)]
+            for check in candidates.keys():
+                if check[0] == i or check[1] == j or (check[0] // 3, check[1] // 3) == (i // 3, j // 3):
+                    if num in candidates[check]:
+                        candidates[check].remove(num)
+            i, j = min(candidates.keys(), key=lambda x: len(candidates[x]))
+            only_one_candidate = len(candidates[(i, j)]) == 1
+        return candidates
 
     def solver(self):
         if len(self.candidates) == 0:
@@ -71,12 +88,13 @@ def test():
               [".", ".", ".", "8", ".", "3", ".", "2", "."],
               [".", ".", ".", ".", ".", ".", ".", ".", "6"],
               [".", ".", ".", "2", "7", "5", "9", ".", "."]]
-    Solution().solveSudoku(sudoku)
+    Solution(sudoku).solve_sudoku()
     for _ in range(9):
         sudoku[_] = "".join(sudoku[_])
 
     assert sudoku == ['519748632', '783652419', '426139875', '357986241', '264317598', '198524367', '975863124',
                       '832491756', '641275983']
+
 
 if __name__ == "__main__":
     test()
