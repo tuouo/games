7fa6b87246f685c68dfc1a735ffa7bf6e3718309
young luo <ltytuotuo@gmail.com>
2021-06-12 18:59:35 +0800

    update from log version

--- nonogram/no2g.py
+++ nonogram/no2g.py
@@ -35,85 +35,72 @@ input data:
 # 1 2 3
 # 2 1 3
 
-virgin, cross, black = 0, 1, 2    # means unknown, not, is black for each cell
+virgin, CROSS, BLACK = 0, 1, 2    # means unknown, not, is BLACK for each cell
 
 """
 
 
-from functools import reduce
-from copy import deepcopy
-
-
 class No2g:
-    VIRGIN, CROSS, BLACK = 0, 1, 2  # means cell is unknown, not, is black
+    VIRGIN, CROSS, BLACK = 0, 1, 2  # means cell is unknown, not, is BLACK
     SHOW = {VIRGIN: "?", CROSS: " ", BLACK: "*"}
 
     @classmethod
     def no2g(cls, numbers):
         hor_len, ver_len = numbers[0]
         horizontal, vertical = numbers[1: 1 + hor_len], numbers[1 + hor_len:]
-
         table = [[cls.VIRGIN] * ver_len for _ in range(hor_len)]  # Init
-        same, table_pre = False, deepcopy(table)  # for something can't be solved
-        count, limit = 0, hor_len + ver_len
-        hor_ok, ver_ok = [False] * hor_len, [False] * ver_len
-        all_ok = reduce(lambda a, b: a and b, hor_ok + ver_ok)
+        count, change = 0, True
+        hor_change, ver_change = [True] * hor_len, [True] * ver_len
 
         try:
-            while not same and not all_ok and count < limit:
-                for i in range(hor_len):
-                    if not hor_ok[i]:
-                        table[i], hor_ok[i] = cls.scan_line(ver_len, horizontal[i], table[i])
-                for i in range(ver_len):
-                    if not ver_ok[i]:
+            while change:
+                ver_change = [False] * ver_len
+                for i, hor_nums in enumerate(horizontal):
+                    if hor_change[i]:
+                        table[i], ver_change = cls.scan_line(ver_len, hor_nums, table[i], ver_change)
+                hor_change = [False] * hor_len
+                for i, ver_nums in enumerate(vertical):
+                    if ver_change[i]:
                         # transfer vertical to horizontal
-                        line = [table[n][i] for n in range(hor_len)]
-                        new_line, ver_ok[i] = cls.scan_line(hor_len, vertical[i], line)
+                        line_cur = [table[n][i] for n in range(hor_len)]
+                        new_line, hor_change = cls.scan_line(hor_len, ver_nums, line_cur, hor_change)
                         for n in range(hor_len):
                             table[n][i] = new_line[n]
 
-                all_ok = reduce(lambda a, b: a and b, hor_ok + ver_ok)
+                change = any(hor_change + ver_change)
                 count += 1
-                table_pre, same = cls.is_same_table(table, table_pre)
         except Exception as e:
             cls.print_no2g(table, 'Not find a new place to fill')
             raise e
 
-        cls.print_no2g(table, "Total check count is: {}".format(count))
+        cls.print_no2g(table, f"Total check count is: {count}")
         return table
 
     @classmethod
-    def is_same_table(cls, table, pre):
-        same = True
-        for line_n, line_p in zip(table, pre):
-            if line_n != line_p:
-                same = False
-                pre = deepcopy(table)
-                break
-        return pre, same
-
-    @classmethod
-    def scan_line(cls, line_len, tip_nums, line):
+    def scan_line(cls, line_len, tip_nums, line_cur, change_mark):
         if len(tip_nums) == 1:
             if tip_nums[0] == 0:
                 return [cls.CROSS] * line_len, True
             elif tip_nums[0] == line_len:
                 return [cls.BLACK] * line_len, True
 
-        off_left = cls.get_most_left_line(line, line_len, tip_nums)  # possibility of all black to left
-        off_right = cls.get_most_right_line(line, line_len, tip_nums)  # possibility of all black to right
-        new_line = cls.mix_left_right(line, line_len, off_left, off_right)  # get cell suit both left & right
-        line_ok = off_left == off_right
-        if not line_ok:
+        off_left = cls.get_most_left_line(line_cur, line_len, tip_nums)  # possibility of all BLACK to left
+        off_right = cls.get_most_right_line(line_cur, line_len, tip_nums)  # possibility of all BLACK to right
+        line_old = line_cur[::]
+        new_line = cls.mix_left_right(line_cur, line_len, off_left, off_right)  # get cell suit both left & right
+        if off_left != off_right:
             new_line = cls.check_cross(new_line, tip_nums, off_left, off_right)
-        return new_line, line_ok
+        for i in range(line_len):
+            if line_old[i] != new_line[i]:
+                change_mark[i] = True
+        return new_line, change_mark
 
     @classmethod
     def get_most_left_line(cls, line, line_len, tip_nums):
         """
         return [(start, end), (start, end), (start, end),,, ] for each item
         """
-        # greedy, so we need check all black contained
+        # greedy, so we need check all BLACK contained
         last_black = line_len - 1
         while line[last_black] != cls.BLACK and last_black >= 0:
             last_black -= 1
@@ -144,11 +131,11 @@ class No2g:
     def find_next_block_start(cls, line, line_len, block_len, start):
         pos, find = start, False
         while not find:
-            while line[pos] == cls.CROSS:  # skip cross
+            while line[pos] == cls.CROSS:  # skip CROSS
                 pos += 1
             new_pos = pos
             for step in range(1, block_len):
-                if line[pos + step] == cls.CROSS:  # suit block len short than black
+                if line[pos + step] == cls.CROSS:  # suit block len short than BLACK
                     new_pos += (step + 1)  # check current cell's next
                     break
 
@@ -159,17 +146,17 @@ class No2g:
                     pos += 1
                 try:
                     while line[pos + block_len] == cls.BLACK or line[pos - 1] == cls.BLACK:
-                        pos += 1  # black block's next shouldn't be black
+                        pos += 1  # BLACK block's next shouldn't be BLACK
                         while line[pos + block_len] == cls.BLACK:
                             pos += 1
-                        if line[pos - 1] == cls.BLACK:  # black block's pre shouldn't be black
+                        if line[pos - 1] == cls.BLACK:  # BLACK block's pre shouldn't be BLACK
                             if line[pos + block_len] == cls.CROSS:
-                                pos += (block_len + 1)  # range not suit black
+                                pos += (block_len + 1)  # range not suit BLACK
                                 break
                             else:
-                                pos += 1  # continue check next is black
+                                pos += 1  # continue check next is BLACK
                     find = True
-                except Exception as e:  # black block can reach end
+                except Exception as e:  # BLACK block can reach end
                     if (pos + block_len) == line_len and line[pos - 1] != cls.BLACK:
                         find = True
                     else:
@@ -183,10 +170,10 @@ class No2g:
         """
         check pre suit block need move right or not
         """
-        if num == 0:  # if pre still have black, means wrong table
+        if num == 0:  # if pre still have BLACK, means wrong table
             return new_line, num, next_pos
         check_pos = next_pos - 1
-        while line[check_pos] != cls.BLACK:  # find pre black in line
+        while line[check_pos] != cls.BLACK:  # find pre BLACK in line
             if check_pos == 0:
                 return new_line, num, next_pos
             check_pos -= 1
@@ -200,7 +187,7 @@ class No2g:
             pre_block_end -= 1
         new_line[pre_block_end - pre_block_len + 1:pre_block_end + 1] = [cls.VIRGIN] * pre_block_len
 
-        # check_pos means black pos to handle in line now, make sure block include it.
+        # check_pos means BLACK pos to handle in line now, make sure block include it.
         next_pos = cls.find_next_block_start(line, len(line), tip_nums[check_num], check_pos - pre_block_len + 1)
         return cls.check_before(tip_nums, check_num, line, new_line, next_pos)
 
@@ -225,7 +212,7 @@ class No2g:
     @classmethod
     def check_cross(cls, new_line, tip_nums, most_left, most_right):
         """
-        check cross: if virgin block'len less than black block which may appear, must be cross
+        check CROSS: if virgin block'len less than BLACK block which may appear, must be CROSS
         """
         off, block_len = most_left[0][1] + 1, 1
         while off < most_right[-1][0]:
@@ -266,11 +253,7 @@ if __name__ == '__main__':
     with open("test.txt", 'r') as r:
         data = r.read()
 
-    result = []
     reg = re.compile(r'(\d+)')
-    for line_date in data.split('\n'):
-        data = reg.findall(line_date)
-        if data:
-            result.append(list(map(int, data)))
+    description_data = [list(map(int, d)) for line_data in data.splitlines() if (d := reg.findall(line_data))]
     game = No2g()
-    game.no2g(result)
+    game.no2g(description_data)
